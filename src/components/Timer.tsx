a7b50f1ffb94ead2c314a32266bea4fe8bcd6d9f
Nina Koh <ninabk14@gmail.com>
2024-12-06 17:56:36 -0800

    Create Timer.tsx

---
+++ src/components/Timer.tsx
@@ -0,0 +1,26 @@
+import { Devvit, useState, useInterval } from '@devvit/public-api';
+
+function getCurrentTime() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+Devvit.addCustomPostType({
+  name: 'Timer',
+  render: (context) => {
+    const [time, setTime] = useState(() => getCurrentTime());
+    const tick = () => setTime(() => getCurrentTime());
+    useInterval(tick, 1000).start();
+
+    return (
+      <vstack alignment="center middle" height="100%">
+        <text size="xxlarge">{time}</text>
+      </vstack>
+    );
+  },
+});
+
+export default Devvit;
